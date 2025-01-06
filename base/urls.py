70f8e806cb1f06fdaffc729c3e0e1aff8ea7742f
prathmesh1602 <prathmeshshinde4516@gmail.com>
2025-01-06 13:41:12 +0530

    Added Functionality

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
+    path('create-room/', views.creatroom, name="creat-room"),
+    path('update-room/<str:pk>', views.updateroom, name="update-room"),
+    path('delete-room/<str:pk>', views.deleteroom, name="delete-room"),
 ]
